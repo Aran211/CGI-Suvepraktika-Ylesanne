e92b6f02b3d30609f6dd18df4097f01dd91db21e
Ron-Aran Paju <ron-aran.paju@ut.ee>
2024-03-03 21:25:27 +0200

    Projekt algseisus

---
+++ src/main/java/com/example/cgi_suvepraktika/CgiSuvepraktikaYlesanneApplication.java
@@ -0,0 +1,13 @@
+package com.example.cgi_suvepraktika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CgiSuvepraktikaYlesanneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CgiSuvepraktikaYlesanneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cgi_suvepraktika/CgiSuvepraktikaYlesanneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cgi_suvepraktika;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CgiSuvepraktikaYlesanneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
